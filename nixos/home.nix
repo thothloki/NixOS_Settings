2e56ce5c976c035025416c63861e49044d2fe5e1
thothloki <ahoertsch@gmail.com>
2023-12-25 04:41:08 +0000

    Add files via upload

---
+++ nixos/home.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  programs.home-manager.enable = true;
+
+  
+  home.username = "thothloki";
+  home.homeDirectory = "/home/thothloki";
+  home.stateVersion = "23.11";
+
+  imports = [
+  	./cinnamon.nix
+  ];
+}
