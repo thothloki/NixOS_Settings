9386d8f8643044bb563a8eb2f89f0e3cf69166d8
thothloki <ahoertsch@gmail.com>
2023-12-27 04:54:19 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
+#! /bin/bash
+echo "Copying .nix files to /etc/nixos"
+
 cp nixos/cinnamon.nix /etc/nixos/
 cp nixos/configuration.nix /etc/nixos/
 cp nixos/home.nix /etc/nixos/
 
+echo "Let's build the system!!!"
 sudo nixos-rebuild switch
 
+echo "Let's get some app configs copied over"
 cp FreeCAD/config/user.cfg ~/.config/FreeCAD
